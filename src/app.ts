e815e839c35dce75096081319800622b696c8607
JonathanP4 <jonathanpetersen2016@gmail.com>
2023-11-24 21:04:09 -0300

    hello

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,7 @@ app.use((req, res, next) => {
         `Access-Control-Allow-Headers`,
         `X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version`
     );
+    next();
 });
 
 app.get("/hello", (req, res, next) => {
