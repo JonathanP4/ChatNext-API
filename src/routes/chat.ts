ac5c220851b96b618b94aff51d0aef8d59aa1dcc
JonathanP4 <jonathanpetersen2016@gmail.com>
2023-11-26 07:58:05 -0300

    a

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -5,7 +5,7 @@ import isAuth from "../middleware/isAuth";
 const router = Router();
 
 router.get("/profile", isAuth, chatControllers.getProfile);
-router.get("/users", isAuth, chatControllers.getUsers);
+router.get("/users", chatControllers.getUsers);
 router.get("/messages", isAuth, chatControllers.getMessages);
 
 router.get("/user/:userId", isAuth, chatControllers.getUser);
