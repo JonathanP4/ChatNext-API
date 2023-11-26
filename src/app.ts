3bdcca9c255a2aeef6fdc9a097e667998c076cea
JonathanP4 <jonathanpetersen2016@gmail.com>
2023-11-26 09:13:06 -0300

    isAuth removed temp

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,7 @@ app.use("/chat", chatRoutes);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     res.status(500).json({ message: err.message });
+    next();
 });
 
 (async function main() {
